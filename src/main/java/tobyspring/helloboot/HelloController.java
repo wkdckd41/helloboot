7a7d19279b9460c763d25b492b1f96a21d3306cf
wkdckd41 <wkdckd41@gmail.com>
2023-09-13 23:51:09 +0900

    섹션3 독립 실행형 서블릿 애플리케이션 - Containerless 개발준비

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,8 +3,6 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.*;
-
 @RestController
 public class HelloController {
     @GetMapping("/hello")
