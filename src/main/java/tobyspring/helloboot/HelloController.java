418fb7fc9ed0bbb131652f2e14631041b46f1a2c
wkdckd41 <wkdckd41@gmail.com>
2023-09-16 23:50:55 +0900

    섹션4 독립 실행형 스프링 애플리케이션 - 의존 오브젝트 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        // Objects.requireNonNull() - null 이라면 예외를 던지고 null 이 아니라면 값을 그대로 리턴해준다.
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
