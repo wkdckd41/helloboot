7c90a500dccaeec6f648ae6a523a820fa0d39bff
wkdckd41 <wkdckd41@gmail.com>
2023-09-17 23:42:32 +0900

    섹션4 독립 실행형 스프링 애플리케이션 - 자바코드 구성정보 사용

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -2,14 +2,26 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HelloBootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigServletWebApplicationContext applicationContext = new AnnotationConfigServletWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -22,10 +34,8 @@ public class HelloBootApplication {
 
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(HelloBootApplication.class);
         applicationContext.refresh();
-
 
     }
 }
