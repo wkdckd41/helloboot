a7e6ecc59630f53342210901c720f9da7809dda3
wkdckd41 <wkdckd41@gmail.com>
2023-10-05 22:55:31 +0900

    섹션10 스프링부트 자세히 살펴보기 - 스프링부트의 자동구성과 테스트로 전환

--- src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -2,11 +2,14 @@ package tobyspring.helloboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@HellobootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
 public class HelloRepositoryTest {
     @Autowired JdbcTemplate jdbcTemplate;
     @Autowired HelloRepository helloRepository;
@@ -17,7 +20,7 @@ public class HelloRepositoryTest {
     }
 
     @Test
-    void increaseCount() {
+    void incraseCount() {
         assertThat(helloRepository.countOf("Toby")).isEqualTo(0);
 
         helloRepository.increaseCount("Toby");
